7014c6889891a0660fbdb3e7d88a2906bcd084f4
lam-vy-mulodo <lam.vy@mulodo.com>
2014-11-17 15:47:38 +0700

    add sql script for create db

---
+++ src/db/blog_sql_script.sql
@@ -0,0 +1,49 @@
+/**************CREATE DATABASE BLOG *********************/
+CREATE DATABASE blog;
+use blog;
+
+
+/*************CREATE TABLE USERS**********************/
+CREATE TABLE IF NOT EXISTS users(
+id INT(11) not null auto_increment,
+username varchar(50) not null,
+password varchar(255) not null,
+email varchar(100) not null,
+lastname varchar(50) not null,
+firstname varchar(50) not null,
+last_login int(11) not null,
+login_hash varchar(255) not null,
+status int(1) not null default 1,
+primary key(id)
+)engine=InnoDB charset='utf8' auto_increment=1;
+
+
+
+/*************CREATE TABLE POSTS**********************/
+
+CREATE TABLE IF NOT EXISTS posts(
+id int(11) not null auto_increment primary key,
+title varchar(255) character set utf8 not null,
+content text character set utf8 not null,
+created_at int(11) not null,
+modified_at int(11) not null,
+author_id int(11) not null,
+status int(1) not null default'1'
+)engine=InnoDB charset='utf8' auto_increment=1;
+
+
+/*************CREATE TABLE COMMENTS*******************/
+
+CREATE TABLE IF NOT EXISTS comments(
+id int(11) not null auto_increment primary key,
+content text character set utf8 not null,
+created_at int(11) not null,
+modified_at int(11) not null,
+author_id int(11) not null,
+post_id int(11) not null
+)engine=InnoDB charset='utf8' auto_increment=1;
+
+/***********************FK key************************/
+alter table posts add foreign key(author_id) references users(id);
+alter table comments add foreign key(author_id) references users(id);
+alter table comments add foreign key(post_id) references posts(id);
