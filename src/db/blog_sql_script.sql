94f357cdafc252c0da93ff814e6eb3b65e166174
lam-vy-mulodo <lam.vy@mulodo.com>
2014-11-17 16:52:11 +0700

    edit tables name without 's'

--- src/db/blog_sql_script.sql
+++ src/db/blog_sql_script.sql
@@ -3,8 +3,8 @@ CREATE DATABASE blog CHARACTER SET utf8 COLLATE utf8_general_ci;
 use blog;
 
 
-/*************CREATE TABLE USERS**********************/
-CREATE TABLE IF NOT EXISTS users(
+/*************CREATE TABLE user**********************/
+CREATE TABLE IF NOT EXISTS user(
 id INT(11) not null auto_increment,
 username varchar(50) not null,
 password varchar(255) not null,
@@ -21,9 +21,9 @@ primary key(id)
 
 
 
-/*************CREATE TABLE POSTS**********************/
+/*************CREATE TABLE post**********************/
 
-CREATE TABLE IF NOT EXISTS posts(
+CREATE TABLE IF NOT EXISTS post(
 id int(11) not null auto_increment primary key,
 title varchar(255) character set utf8 not null,
 content text character set utf8 not null,
@@ -34,9 +34,9 @@ status int(1) not null default'1'
 )engine=InnoDB charset='utf8' auto_increment=1;
 
 
-/*************CREATE TABLE COMMENTS*******************/
+/*************CREATE TABLE comment*******************/
 
-CREATE TABLE IF NOT EXISTS comments(
+CREATE TABLE IF NOT EXISTS comment(
 id int(11) not null auto_increment primary key,
 content text character set utf8 not null,
 created_at int(11) not null,
@@ -46,6 +46,6 @@ post_id int(11) not null
 )engine=InnoDB charset='utf8' auto_increment=1;
 
 /***********************FK key************************/
-alter table posts add foreign key(author_id) references users(id);
-alter table comments add foreign key(author_id) references users(id);
-alter table comments add foreign key(post_id) references posts(id);
+alter table post add foreign key(author_id) references user(id);
+alter table comment add foreign key(author_id) references user(id);
+alter table comment add foreign key(post_id) references post(id);
