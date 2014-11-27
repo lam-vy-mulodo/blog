efa027f23841894eda2420aa858f245f35c0ce03
lam-vy-mulodo <lam.vy@mulodo.com>
2014-11-27 14:06:12 +0700

    Config db test change to blog_test

--- src/fuel/app/config/test/db.php
+++ src/fuel/app/config/test/db.php
@@ -8,7 +8,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=blog',
+			'dsn'        => 'mysql:host=localhost;dbname=blog_test',
 			'username'   => 'root',
 			'password'   => '1234',
 		),
