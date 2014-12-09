266091cc2bc2cad69d87916fdefd1c7459660876
lam-vy-mulodo <lam.vy@mulodo.com>
2014-12-09 17:09:07 +0700

    Update code for get user_info

--- src/fuel/app/config/routes.php
+++ src/fuel/app/config/routes.php
@@ -4,11 +4,13 @@ return array(
 	'_root_'  => 'welcome/index',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
 	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+	'hello(/:name)?' => array( 'welcome/hello', 'name' => 'hello' ),
 	//config routes for users controller
 		
-	'(:version)/users' => array(array('GET', new Route('$1/user/index')),array('POST', new Route('$1/user/register')),array('PUT', new Route('$1/user/update')
+	'(:version)/users' => array( array( 'GET', new Route('$1/user/index') ), array( 'POST', new Route('$1/user/register') ), 
+			array( 'PUT', new Route( '$1/user/update_user' )
 )),
-	'(:version)/users/login' => array(array('POST', new Route('$1/user/login'))) ,
-	'(:version)/users/logout' => array(array('PUT', new Route('$1/user/logout'))) ,
+	'(:version)/users/login' => array( array( 'POST', new Route( '$1/user/login' ))) ,
+	'(:version)/users/logout' => array( array( 'PUT', new Route( '$1/user/logout' ))) ,
+	'(:version)/users/(:num)' => array( array( 'GET', new Route( '$1/user/user_info/$3' ))) ,
 );
