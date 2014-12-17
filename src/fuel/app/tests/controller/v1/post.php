fdedbb39350264ed61dc097164656fec17f98f70
lam-vy-mulodo <lam.vy@mulodo.com>
2014-12-17 10:24:38 +0700

    Add 2 new file post for unit test about post

---
+++ src/fuel/app/tests/controller/v1/post.php
@@ -0,0 +1,29 @@
+<?php
+<?php
+
+/**
+ * The Test for Post Controller version 1.
+* @extends TestCase
+*
+* @author Lam Vy
+* @group Post
+*/
+class Test_Controller_V1_Post extends TestCase {
+
+	/**
+	 * called before each test method
+	 * @before
+	 */
+	public function setUp() {
+			
+	}
+	
+	/**
+	 * called after each test method
+	 * @after
+	 */
+	public function tearDown() {
+		//to do
+	
+	}
+}
---
+++ src/fuel/app/tests/model/v1/post.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * The Test for Post Model version 1.
+ * @extends TestCase
+ *
+ * @author Lam Vy
+ * @group Post
+ */
+class Test_Model_V1_Post extends TestCase {
+
+	/**
+	 * called before each test method
+	 * @before
+	 */
+	public function setUp() {
+			
+	}
+	
+	/**
+	 * called after each test method
+	 * @after
+	 */
+	public function tearDown() {
+		//to do
+	
+	}
+}
