f4d2ead00034dfe916a0fb526165d070fc105fb8
lam-vy-mulodo <lam.vy@mulodo.com>
2014-12-19 13:38:28 +0700

    Upload unit testing for active/inactive a post

--- src/fuel/app/tests/model/v1/post.php
+++ src/fuel/app/tests/model/v1/post.php
@@ -141,4 +141,96 @@ class Test_Model_V1_Post extends TestCase {
 		return $test_data;
 	}
 	
+	/**
+	 * funtion to test update status for post is ok
+	 * @param : author_id, post_id and status
+	 * compare with expected data
+	 * @group update_status_ok
+	 * @dataProvider status_change_ok_provider
+	 */
+	public function test_change_status_post($test_data) {
+		//call update status in the model
+		$data = Post::update_status($test_data['post_id'], $test_data['status'], $test_data['author_id']);
+		
+		//compare with the data expected
+		$this->assertEquals($test_data['post_id'], $data['id']);
+		$this->assertEquals($test_data['title'], $data['title']);		
+		$this->assertEquals($test_data['created_at'], $data['created_at']);
+		$this->assertEquals($test_data['modified_at'], $data['modified_at']);
+	}
+	
+	/**
+	 * Define test data for check login not ok
+	 *
+	 * @return array Test data
+	 */
+	public function status_change_ok_provider() {
+		$test_data = array();
+		//deactive post\
+		$test_data[][] = array(
+			'post_id' => '17',
+			'author_id' => '89',
+			'status'	=>	'0',
+			'title' => 'title for unit testing in controller',
+			'created_at' => '1418798556',
+			'modified_at' => '1418798556'
+		);
+		
+		//active post
+		$test_data[][] = array(
+				'post_id' => '17',
+				'author_id' => '89',
+				'status'	=>	'1',
+				'title' => 'title for unit testing in controller',
+				'created_at' => '1418798556',
+				'modified_at' => '1418798556'
+		);
+		return $test_data;
+	}
+	
+	/**
+	 * funtion to test update status for post is not ok
+	 * @param : author_id, post_id and status
+	 * compare with false
+	 * @group update_status_notok
+	 * @dataProvider status_change_notok_provider
+	 */
+	public function test_change_status_post_notok($test_data) {
+		//call update status in the model
+		$rs = Post::update_status($test_data['post_id'], $test_data['status'], $test_data['author_id']);
+	
+		//compare with the data expected
+		$this->assertEquals(false, $rs);
+		
+	}
+	/**
+	 * Define test data for check login not ok
+	 *
+	 * @return array Test data
+	 */
+	public function status_change_notok_provider() {
+		$test_data = array();
+		//active the post was actived
+		$test_data[][] = array(
+				'post_id' => '6',
+				'author_id' => '30',
+				'status'	=>	'1'
+		);
+	
+		//inactive post was inactived
+		$test_data[][] = array(
+				'post_id' => '1',
+				'author_id' => '30',
+				'status'	=>	'0',
+				
+		);
+		//active the post when author id wrong
+		$test_data[][] = array(
+				'post_id' => '1',
+				'author_id' => '31',//true is 30
+				'status'	=>	'1',
+		
+		);
+		return $test_data;
+	}
 }
